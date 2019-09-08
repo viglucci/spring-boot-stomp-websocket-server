eb78fa3b39c03482c99d039bd98bb663fa1bab3d
kevin viglucci <kviglucci@gmail.com>
2019-09-07 21:39:26 -0500

    rename some classes and remove client directory

---
+++ src/main/java/com/solidice/websockettestserver/http/websocket/controllers/PingPongController.java
@@ -0,0 +1,8 @@
+package com.solidice.websockettestserver.http.websocket.controllers;
+
+/**
+ * @author Kevin
+ * @since 9/7/2019
+ */
+public class PingPongController {
+}
---
+++ src/main/java/com/solidice/websockettestserver/models/PongResponse.java
@@ -0,0 +1,13 @@
+package com.solidice.websockettestserver.models;
+
+/**
+ * @author Kevin
+ * @since 9/7/2019
+ */
+public class PongResposne {
+	private String content;
+
+	public PongResposne() {
+		this.content = "pong";
+	}
+}
